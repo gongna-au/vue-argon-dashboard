3a2acedfa9e211ac76ed816798559b3827bf20b5
gongna <2036479155@qq.com>
2024-03-01 17:40:32 +0800

    feat:优化交易按钮

--- src/views/components/TransactionCard.vue
+++ src/views/components/TransactionCard.vue
@@ -3,7 +3,7 @@
     <div class="card-header pb-0 px-3">
       <div class="row">
         <div class="col-md-6">
-          <h6 class="mb-0">Transaction's 银行卡支出信息</h6>
+          <h6 class="mb-0">Transaction's 账户自动扣款信息</h6>
         </div>
         <div class="col-md-6 d-flex justify-content-end align-items-center">
           <i class="far fa-calendar-alt me-2" aria-hidden="true"></i>
@@ -14,114 +14,58 @@
     <div class="card-body pt-4 p-3">
       <h6 class="text-uppercase text-body text-xs font-weight-bolder mb-3">Newest</h6>
       <ul class="list-group">
-        <li
-          class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
-        >
-        <div class="d-flex align-items-center">
-            <argon-button
-              color="success"
-              variant="outline"
-              size="sm"
-              class="btn-icon-only btn-rounded mb-0 me-3 d-flex align-items-center justify-content-center"
-            >
-              <i class="fas fa-arrow-up" aria-hidden="true"></i>
-            </argon-button>
-            <div class="d-flex flex-column">
-              <h6 class="mb-1 text-dark text-sm">4123 4567 8901 7852</h6>
-              <span class="text-xs">27 March 2020, at 04:30 AM</span>
-            </div>
-          </div>
-          <div
-            class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold"
-          >+ $ 2,000</div>
-        </li>
+            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
+                  v-for="(transaction, index) in Ttransactions"
+                  :key="index"
+                >
+                  <div class="d-flex align-items-center">
+                    <argon-button
+                      :color="transaction.status === 'success' ? 'success' : 'dark'"
+                      variant="outline"
+                      size="sm"
+                      class="btn-icon-only btn-rounded mb-0 me-3 d-flex align-items-center justify-content-center"
+                    >
+                      <i :class="transaction.status === 'success' ? 'fas fa-arrow-up' : 'fas fa-exclamation'" aria-hidden="true"></i>
+                    </argon-button>
+                    <div class="d-flex flex-column">
+                      <h6 class="mb-1 text-dark text-sm">{{ transaction.date }}</h6>
+                      <span v-if="transaction.cardNumber" class="text-xs">{{ transaction.cardNumber }}</span>
+                    </div>
+                  </div>
+                  <div
+                    class="d-flex align-items-center"
+                    :class="transaction.status === 'success' ? 'text-success text-gradient' : 'text-dark'"
+                    text-sm font-weight-bold
+                  >-${{ transaction.amount  }}</div>
+            </li>
       </ul>
      
       <h6 class="text-uppercase text-body text-xs font-weight-bolder my-3">Yesterday</h6>
       <ul class="list-group">
-        <li
-          class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
-        >
-          <div class="d-flex align-items-center">
-            <argon-button
-              color="success"
-              variant="outline"
-              size="sm"
-              class="btn-icon-only btn-rounded mb-0 me-3 d-flex align-items-center justify-content-center"
-            >
-              <i class="fas fa-arrow-up" aria-hidden="true"></i>
-            </argon-button>
-            <div class="d-flex flex-column">
-              <h6 class="mb-1 text-dark text-sm">4123 4567 8901 7852</h6>
-              <span class="text-xs">26 March 2020, at 13:45 PM</span>
-            </div>
-          </div>
-          <div
-            class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold"
-          >+ $ 750</div>
-        </li>
-        
-        <li
-          class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
-        >
-          <div class="d-flex align-items-center">
-            <argon-button
-              color="success"
-              variant="outline"
-              size="sm"
-              class="btn-icon-only btn-rounded mb-0 me-3 d-flex align-items-center justify-content-center"
-            >
-              <i class="fas fa-arrow-up" aria-hidden="true"></i>
-            </argon-button>
-            <div class="d-flex flex-column">
-              <h6 class="mb-1 text-dark text-sm">4532 9876 5432 5248</h6>
-              <span class="text-xs">26 March 2020, at 12:30 PM</span>
-            </div>
-          </div>
-          <div
-            class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold"
-          >+ $ 1,000</div>
-        </li>
-        <li
-          class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
-        >
-          <div class="d-flex align-items-center">
-            <argon-button
-              color="success"
-              variant="outline"
-              size="sm"
-              class="btn-icon-only btn-rounded mb-0 me-3 d-flex align-items-center justify-content-center"
-            >
-              <i class="fas fa-arrow-up" aria-hidden="true"></i>
-            </argon-button>
-            <div class="d-flex flex-column">
-              <h6 class="mb-1 text-dark text-sm">4532 9876 5432 5248</h6>
-              <span class="text-xs">26 March 2020, at 08:30 AM</span>
-            </div>
-          </div>
-          <div
-            class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold"
-          >+ $ 2,500</div>
-        </li>
-        <li
-          class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
-        >
-          <div class="d-flex align-items-center">
-            <argon-button
-              color="dark"
-              variant="outline"
-              size="sm"
-              class="btn-icon-only btn-rounded mb-0 me-3 d-flex align-items-center justify-content-center"
-            >
-              <i class="fas fa-exclamation" aria-hidden="true"></i>
-            </argon-button>
-            <div class="d-flex flex-column">
-              <h6 class="mb-1 text-dark text-sm">4532 9876 5432 5248</h6>
-              <span class="text-xs">26 March 2020, at 05:00 AM</span>
-            </div>
-          </div>
-          <div class="d-flex align-items-center text-dark text-sm font-weight-bold">Pending</div>
-        </li>
+            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
+                  v-for="(transaction, index) in Ytransactions"
+                  :key="index"
+                >
+                  <div class="d-flex align-items-center">
+                    <argon-button
+                      :color="transaction.status === 'success' ? 'success' : 'dark'"
+                      variant="outline"
+                      size="sm"
+                      class="btn-icon-only btn-rounded mb-0 me-3 d-flex align-items-center justify-content-center"
+                    >
+                      <i :class="transaction.status === 'success' ? 'fas fa-arrow-up' : 'fas fa-exclamation'" aria-hidden="true"></i>
+                    </argon-button>
+                    <div class="d-flex flex-column">
+                      <h6 class="mb-1 text-dark text-sm">{{ transaction.date }}</h6>
+                      <span v-if="transaction.cardNumber" class="text-xs">{{ transaction.cardNumber }}</span>
+                    </div>
+                  </div>
+                  <div
+                    class="d-flex align-items-center"
+                    :class="transaction.status === 'success' ? 'text-success text-gradient' : 'text-dark'"
+                    text-sm font-weight-bold
+                  >-${{ transaction.amount  }}</div>
+            </li>
       </ul>
     </div>
   </div>
@@ -135,5 +79,47 @@ export default {
   components: {
     ArgonButton,
   },
+  data() {
+  return {
+    Ttransactions: [
+          {
+            date: "28 March 2024, at 04:30 AM",
+            amount: 2000,
+            status: "success", // 可以是 'success', 'pending', 等
+          },
+          {
+            date: "28 March 2024, at 13:45 PM",
+            amount: 750,
+            status: "success",
+          },
+          {
+            date: "28 March 2024, at 12:30 PM",
+            amount: 1000,
+            status: "success",
+          },
+      // 其他交易记录...
+      ],
+    Ytransactions: [
+          {
+            date: "27 March 2024, at 04:30 AM",
+            amount: 2000,
+            status: "success", // 可以是 'success', 'pending', 等
+          },
+          {
+            date: "27 March 2024, at 13:45 PM",
+            amount: 750,
+            status: "success",
+          },
+          {
+            date: "27 March 2024, at 12:30 PM",
+            amount: 1000,
+            status: "success",
+          },
+      // 其他交易记录...
+      ],
+    };
+  },
+
 };
+
 </script>
